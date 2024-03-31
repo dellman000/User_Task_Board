226717197d12e60b0f2863dd56bf8e5c30f660d6
kendell <kenren@udel.edu>
2024-03-31 15:41:56 -0400

    working backend data and remove item button

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,6 @@ form.addEventListener('submit', createTaskCard)
 //let MakeTaskBtn=document.getElementById('MakeTaskBtn');
 //MakeTaskBtn.addEventListener('submit',createTaskCard)
 
-// renderSingleTask()
 
 $('.TaskCard').draggable({
     zIndex:100,
@@ -27,69 +26,130 @@ $('#in-progress-cards ,#done-cards, #todo-cards ').droppable({
     // accept:'.TaskCard',
     drop:function(dropEvent,ui){
         let Box=dropEvent.target;
-        //  let Box = document.getElementById('in-progress-cards')
+        const destination =Box.dataset.storage
         let item=ui.draggable[0]
+        let itemObjectID=item.dataset.objectid
+        let Lastlocation=item.dataset.storage;
+
         $(item).detach().css({top:0,left:0}).appendTo(Box)
         // Box.remove()   
         // console.log(item)
-        console.log(Box)
+        let OBJ=removeObject(itemObjectID,Lastlocation)
+        pushItemToStorage(OBJ,destination)
+        item.dataset.storage=destination
+        console.log(destination)
+        
     }
 })
 
+renderTaskList()
 
-
-
+function removeObject(itemObjectID,Lastlocation){
+    let Storage=JSON.parse( localStorage.getItem(Lastlocation));
+    let poppedOBJ;
+    // this should not work 
+    Storage= Storage.filter(element => {
+            if(itemObjectID !=element.TaskID){
+                    return true;
+            }else{
+                
+                poppedOBJ=element
+                return false
+            }
+        //return itemObjectID !=element.TaskID
+    });
+  
+    //console.log(Storage)
+    //console.log(poppedOBJ)
+    Storage=JSON.stringify(Storage)
+    localStorage.setItem(Lastlocation,Storage)
+    return poppedOBJ
+    // Storage.filter()
+}
 
 
 
 // will show the add Task Model
 function ToggleTaskModel() {
-    let Model = document.getElementById('myModal')
-    Model.style.display = 'flex';
+    // let Model = document.getElementById('myModal')
+    model.style.display = 'flex';
 }
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
+function generateTaskId(Title) {
+    const max=1000;
+return Title+Math.floor(Math.random() * max)
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
     task.preventDefault()
-    console.log('Created task')
+    //console.log('Created task')
     let Title = document.getElementById('Title')
     let DueDate = document.getElementById('Due')
     let Description = document.getElementById('Description')
 
     let newOBJ={ 
+        TaskID:generateTaskId(Title.value),
         TitleRender:Title.value,
         DueDateRender:DueDate.value,
         DescriptionRender:Description.value
     }
-    renderSingleTask(newOBJ)
-        
-    
-
+    pushItemToStorage(newOBJ,"ToDoList")
+    renderSingleTask(newOBJ,"ToDoList")
     // console.log(Title.value)
     // console.log(DueDate.value)
     // console.log(Description.value)
-
-
 }
 
+
+function pushItemToStorage(OBJ,location){
+   let Storage=JSON.parse( localStorage.getItem(location))||[]
+    Storage.push(OBJ)
+    //console.log(Storage)
+    Storage=JSON.stringify(Storage)
+    //console.log(Storage)
+    localStorage.setItem(location,Storage)
+
+//    console.log(Storage)
+}
+
+
 //will render a single task to the screen
-function renderSingleTask(OBJ){
-let TodoCards=document.getElementById('todo-cards')
-// console.log(OBJ)
+function renderSingleTask(OBJ,location){
+    let DivLocationID;
+    if(location=='ToDoList'){
+        DivLocationID='todo-cards'
+    }else if(location=='ProgressList'){
+        DivLocationID='in-progress-cards'
+    }else if(location=='Done'){
+        DivLocationID='done-cards'
+    }
+
+
+let TodoCards=document.getElementById(DivLocationID)
+ console.log(TodoCards)
 let Box =document.createElement('div')
-Box.classList='TaskCard'
+Box.classList='TaskCard';
+Box.dataset.objectid=OBJ.TaskID;
+Box.dataset.storage=location
 Box.insertAdjacentHTML('beforeend',`
     
         <h3>${OBJ.TitleRender}</h3>
         <div> ${OBJ.DueDateRender} </div>
         <p> ${OBJ.DescriptionRender}</p>
-    
+        <button class="btn btn-danger">
+        remove
+      </button>
 
 `)
+console.log(Box.querySelector('button') )
+Box.querySelector('button').addEventListener('click',function (eventOBJ){
+    console.log(eventOBJ.target.parentNode)
+    eventOBJ.target.parentNode.remove()
+    removeObject(OBJ.TaskID,location)
+})
+
+
 TodoCards.appendChild(Box)
 $(Box).draggable({
     zIndex:100,
@@ -99,7 +159,25 @@ $(Box).draggable({
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    let ToDoList=JSON.parse(localStorage.getItem('ToDoList')) || []
+    let ProgressList=JSON.parse(localStorage.getItem('ProgressList')) || []
+    let DoneList=JSON.parse(localStorage.getItem('Done')) || []
+    // todo-cards
+    //in-progress-cards
+    //done-cards
+    ToDoList.forEach(element => {
+        renderSingleTask(element,'ToDoList')
+    });
+    ProgressList.forEach(element => {
+        renderSingleTask(element,'ProgressList')
+    });
+    DoneList.forEach(element => {
+        renderSingleTask(element,'Done')
+    });
 
+    console.log(ToDoList)
+    console.log(ProgressList)
+    console.log(DoneList)
 }
 
 // Todo: create a function to handle adding a new task
