55d6d0b0f16b8c0171db17f55a323be40bacf884
kendell <kenren@udel.edu>
2024-03-30 20:39:13 -0400

    added dragables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,49 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Get the <span> element that closes the modal
 var span = document.querySelector(".close");
-let model =document.getElementById('myModal')
-$('#TaskForm').on('click',ToggleTaskModel)
-  span.addEventListener('click',function() {
+let model = document.getElementById('myModal')
+$('#TaskForm').on('click', ToggleTaskModel)
+span.addEventListener('click', function () {
     model.style.display = "none";
-  })
+})
+
+const form = document.querySelector('form')
+form.addEventListener('submit', createTaskCard)
+
+//let MakeTaskBtn=document.getElementById('MakeTaskBtn');
+//MakeTaskBtn.addEventListener('submit',createTaskCard)
+
+renderSingleTask()
+
+$('.TaskCard').draggable({
+    zIndex:100,
+    revert:true
+})
+
+$('#in-progress-cards ,#done-cards, #todo-cards ').droppable({
+    accept:'.TaskCard',
+    drop:function(dropEvent,ui){
+        let Box=dropEvent.target;
+        //  let Box = document.getElementById('in-progress-cards')
+        let item=ui.draggable[0]
+        $(item).detach().css({top:0,left:0}).appendTo(Box)
+        // Box.remove()   
+        // console.log(item)
+        console.log(Box)
+    }
+})
+
+
+
+
+
+
 
 // will show the add Task Model
-function ToggleTaskModel(){
-    let Model =document.getElementById('myModal')
-    Model.style.display='flex';
+function ToggleTaskModel() {
+    let Model = document.getElementById('myModal')
+    Model.style.display = 'flex';
 }
-
-
-
-
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -26,8 +54,39 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    task.preventDefault()
+    console.log('Created task')
+    let Title = document.getElementById('Title')
+    let DueDate = document.getElementById('Due')
+    let Description = document.getElementById('Description')
+
+    let newOBJ={ 
+        TitleRender:Title,
+        DueDateRender:DueDate,
+        DescriptionRender:Description
+    }
+    renderSingleTask(newOBJ)
+        
+    
+
+    console.log(Title.value)
+    console.log(DueDate.value)
+    console.log(Description.value)
+
 
 }
+
+//will render a single task to the screen
+function renderSingleTask(){
+let Box=document.getElementById('todo-cards')
+Box.insertAdjacentHTML('beforeend',`
+<div class="TaskCard">
+                  <h3>Item 1</h3>
+                </div>
+
+`)
+}
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -35,12 +94,12 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
